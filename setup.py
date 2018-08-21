cc642054d3734b32224f568bfe37de8816ab5641
iconix <narhodes1@gmail.com>
2018-08-21 18:58:18 +0000

    setuptools.find_packages() doesn't seem to work - replace

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="pytorchtextvae",
-    version="0.0.1",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/iconix/pytorch-text-vae",
-    packages=setuptools.find_packages(),
+setup(
+    name = "pytorchtextvae",
+    version = "0.0.1",
+    description = "A partial reimplementation of \"Generating Sentences From a Continuous Space\" by Bowman, Vilnis, Vinyals, Dai, Jozefowicz, Bengio (https://arxiv.org/abs/1511.06349).",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    license = "MIT",
+    url = "https://github.com/iconix/pytorch-text-vae",
+    packages = [ 'pytorchtextvae' ],
+    install_requires = [ 'dill', 'fire', 'pytorch', 'unidecode' ],
+    keywords = [ 'deeplearning', 'pytorch', 'vae', 'nlp' ],
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3.6',
+                   'Topic :: Scientific/Engineering :: Artificial Intelligence']
 )
