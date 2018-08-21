5a51b2cddb83b90cdfd6a687be105acbcb580267
iconix <narhodes1@gmail.com>
2018-08-21 04:52:24 +0000

    Update pkg name to remove underscores

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytorch_text_vae",
+    name="pytorchtextvae",
     version="0.0.1",
     long_description=long_description,
     long_description_content_type="text/markdown",
