6ea488bce6ff723a9510c5e3542b9d7e064dcf06
iconix <narhodes1@gmail.com>
2018-10-24 12:35:44 +0000

    Update generate.py after train/test split

--- pytorchtextvae/generate.py
+++ pytorchtextvae/generate.py
@@ -21,7 +21,7 @@ def load_model(saved_vae, stored_info, device, cache_path=str(Path('../tmp')), s
     start_load = time.time()
     print(f"Fetching cached info at {cache_file}")
     with open(cache_file, "rb") as f:
-        input_side, output_side, pairs, dataset, z_size, condition_size, condition_on, decoder_hidden_size, encoder_hidden_size, n_encoder_layers = pickle.load(f)
+        dataset, z_size, condition_size, condition_on, decoder_hidden_size, encoder_hidden_size, n_encoder_layers = pickle.load(f)
     end_load = time.time()
     print(f"Cache {cache_file} loaded (load time: {end_load - start_load:.2f}s)")
 
@@ -29,8 +29,8 @@ def load_model(saved_vae, stored_info, device, cache_path=str(Path('../tmp')), s
         print(f"Found saved model {saved_vae}")
         start_load_model = time.time()
 
-        e = model.EncoderRNN(input_side.n_words, encoder_hidden_size, z_size, n_encoder_layers, bidirectional=True)
-        d = model.DecoderRNN(z_size, dataset.n_conditions, condition_size, decoder_hidden_size, input_side.n_words, 1, word_dropout=0)
+        e = model.EncoderRNN(dataset.input_side.n_words, encoder_hidden_size, z_size, n_encoder_layers, bidirectional=True)
+        d = model.DecoderRNN(z_size, dataset.trn_split.n_conditions, condition_size, decoder_hidden_size, dataset.input_side.n_words, 1, word_dropout=0)
         vae = model.VAE(e, d).to(device)
         vae.load_state_dict(torch.load(saved_vae, map_location=lambda storage, loc: storage))
         vae.eval()
@@ -46,33 +46,33 @@ def load_model(saved_vae, stored_info, device, cache_path=str(Path('../tmp')), s
         torch.manual_seed(new_seed)
 
         random_state = np.random.RandomState(new_seed)
-        random_state.shuffle(pairs)
+        #random_state.shuffle(dataset.trn_pairs)
 
-    return vae, input_side, output_side, pairs, dataset, z_size, random_state
+    return vae, dataset, z_size, random_state
 
-def generate(vae, input_side, output_side, pairs, dataset, z_size, random_state, device, condition_inputs=None, max_length=50, num_sample=10, temp=0.75, print_z=False):
+def generate(vae, dataset, z_size, random_state, device, condition_inputs=None, max_length=50, num_sample=10, temp=0.75, print_z=False):
     gens = []
     zs = []
     conditions = []
 
-    if dataset.n_conditions and condition_inputs is not None and not isinstance(condition_inputs, list):
+    if dataset.trn_split.n_conditions > -1 and condition_inputs is not None and not isinstance(condition_inputs, list):
         print(f'[WARNING] condition_inputs provided is of type "{type(condition_inputs).__name__}" but should be of type "list". Continuing with random condition_inputs...')
 
     for i in range(num_sample):
         z = torch.randn(z_size).unsqueeze(0).to(device)
 
-        if dataset.n_conditions:
+        if dataset.trn_split.n_conditions > -1:
             if isinstance(condition_inputs, list):
                 condition = torch.tensor(dataset.encode_conditions(condition_inputs), dtype=torch.float).unsqueeze(0).to(device)
             else:
-                condition = model.random_training_set(pairs, input_side, output_side, random_state, device)[2]
+                condition = model.random_training_set(dataset, random_state, device)[2]
         else:
             condition = None
 
         generated = vae.decoder.generate(z, condition, max_length, temp, device)
-        generated_str = model.float_word_tensor_to_string(output_side, generated)
+        generated_str = model.float_word_tensor_to_string(dataset.output_side, generated)
 
-        EOS_str = f' {output_side.index_to_word(torch.LongTensor([EOS_token]))} '
+        EOS_str = f' {dataset.output_side.index_to_word(torch.LongTensor([EOS_token]))} '
 
         if generated_str.endswith(EOS_str):
             generated_str = generated_str[:-5]
@@ -81,12 +81,12 @@ def generate(vae, input_side, output_side, pairs, dataset, z_size, random_state,
         generated_str = generated_str[::-1]
 
         print('---')
-        if dataset.n_conditions:
+        if dataset.trn_split.n_conditions > -1:
             print(dataset.decode_conditions(condition))
         print(generated_str)
         gens.append(generated_str)
         zs.append(z)
-        if dataset.n_conditions:
+        if dataset.trn_split.n_conditions > -1:
             conditions.append(condition)
         if print_z:
             print(z)
@@ -102,8 +102,8 @@ def run(saved_vae, stored_info, cache_path=str(Path(f'..{os.sep}tmp')), conditio
     DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() and use_cuda else 'cpu')
 
     with torch.no_grad():
-        vae, input_side, output_side, pairs, dataset, z_size, random_state = load_model(saved_vae, stored_info, DEVICE, cache_path, seed)
-        gens, zs, conditions = generate(vae, input_side, output_side, pairs, dataset, z_size, random_state, DEVICE, condition_inputs, max_length, num_sample, temp, print_z)
+        vae, dataset, z_size, random_state = load_model(saved_vae, stored_info, DEVICE, cache_path, seed)
+        gens, zs, conditions = generate(vae, dataset, z_size, random_state, DEVICE, condition_inputs, max_length, num_sample, temp, print_z)
 
 if __name__ == "__main__":
     import fire; fire.Fire(run)
